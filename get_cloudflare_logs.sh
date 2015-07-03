420812d7da4191fe7aeaaf5aa41a719036bfb6dd
ejayesh <ejayesh@hotmail.com>
2015-07-03 15:01:19 +0000

    2

--- get_cloudflare_logs.sh
+++ get_cloudflare_logs.sh
@@ -2,6 +2,14 @@
 
 CurrentDate=`date "+%Y_%m_%d"`
 FileDate=`/bin/date -d "-15 min" "+%Y_%m_%d-%H_%M"`
+CurrentTime=`date "+%H%M"`
+
+#Delete all logs older then 60 days old
+#Will ensure this runs just onces a day at 02:00
+if [ "`date +%H%M`" == "0200" ]
+then
+	find /mnt/logs/cloudflare -maxdepth 1 -type d -mtime +60 -not -path "/mnt/logs/cloudflare/.ssh" -exec echo rmdir {} \;
+fi
 
 [ -d /mnt/logs/cloudflare/$CurrentDate ] || mkdir /mnt/logs/cloudflare/$CurrentDate
 
